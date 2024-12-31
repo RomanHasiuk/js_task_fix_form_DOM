0c235ff8da9933c786eff0c598b35251ec3662df
RomanHasiuk <romanhasiuk1@gmail.com>
2024-12-31 17:12:01 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const parentElement = input.parentElement;
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+
+  const readableName = input.name.replace(/([A-Z])/g, ' $1').trim();
+  const capitalizedName =
+    readableName.charAt(0).toUpperCase() + readableName.slice(1);
+
+  label.textContent = capitalizedName;
+  input.placeholder = capitalizedName;
+  parentElement.prepend(label);
+});
